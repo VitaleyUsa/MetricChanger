97d6e4a3f7ab5c951cab0c8ab2f06c7d3d8485b8
VitaleyUsa <vitaley.usa@gmail.com>
2022-07-25 01:21:53 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.NetworkInformation;
+using System.Diagnostics;
+
+namespace MetricChanger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            NetworkInterface[] allNetworkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            NetworkInterface[] array = allNetworkInterfaces;
+            foreach (NetworkInterface networkInterface in array)
+            {
+                count++;
+                if (networkInterface.Description.Contains("CP-TAP-Windows Adapter V9"))
+                    Process.Start("CMD.exe", "/c netsh.exe interface ipv4 set interface " + networkInterface.Name + " metric=" + count);
+            }
+        }
+    }
+}
